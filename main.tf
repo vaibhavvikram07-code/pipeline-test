8a2d7cec6e7340baf8d96d70118513e74482ecfd
vaibhavvikram07-code <vaibhavvikram07@gmail.com>
2026-09-10 16:20:16 +0530

    state file commited

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example12" {
-  name     = "rg-terraform-manual12"
+resource "azurerm_resource_group" "myRG" {
+  name     = "rg-terraform"
   location = "East US"
 }
